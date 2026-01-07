9cf43e175827ce8bd1af66913bf8e8be2715d6a4
Developer <dev@smarthr.com>
2026-01-07 10:49:46 +0530

    ...

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -3,8 +3,6 @@
 echo "Starting Frontend..."
 echo "BACKEND_URL is set to: $BACKEND_URL"
 
-export PORT=${PORT:-80}
-
 if [ -z "$BACKEND_URL" ]; then
     echo "WARNING: BACKEND_URL is empty! Nginx might fail."
 fi
